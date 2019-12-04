520c5c15a9c2d8686434e146e5378a7f8434c992
wanglei <396844151@qq.com>
2019-12-04 21:10:38 +0800

    博客搭建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  title: 'wl的博客',
+  description: '欢迎访问我的前端日志',
+  lastUpdated: 'Last Updated',
+  // editLinks: true,
+  // editLinkText: "本文源码地址",
+  head: [
+    ['link', { rel: 'icon', href: '/img/favicon.ico' }]
+  ],
+  themeConfig: {
+    repo: 'wanglei041533/wlblog',
+    nav: [
+      { text: '学习心得', link: '/blog/' },
+      { text: '面试总结', link: '/interview/' },
+      // { text: 'Github', link: 'https://github.com/wanglei041533/wlblog' }
+    ],
+    sidebar: {
+      '/blog/': [
+        {
+          title: 'Linux',
+          collapsable: false,
+          children: [
+            'linux'
+          ]
+        },
+        {
+          title: 'js基础',
+          collapsable: false,
+          children: [
+            // 'js1'
+          ]
+        }
+      ],
+      '/interview/': [
+        {
+          title:'js面试题',
+          collapsable: false,
+          children: [
+           'js-amd-cmd'
+          ]
+        },
+        {
+          title:'css面试题',
+          collapsable: false,
+          children: [
+          
+          ]
+        },
+        {
+          title:'html面试题',
+          collapsable: false,
+          children: [
+            
+          ]
+        },
+        {
+          title:'框架面试题',
+          collapsable: false,
+          children: [
+           
+          ]
+        }
+      ]
+    }
+  }
+}
